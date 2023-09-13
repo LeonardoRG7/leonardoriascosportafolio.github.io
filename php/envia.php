e2333ca22caf5619f96fd84005dc40767ffad4bb
Leonardo Riascos <Leonardoriascos82@gmail.com>
2023-09-12 20:11:51 -0500

    change method send emails v1.1

--- php/envia.php
+++ php/envia.php
@@ -5,7 +5,8 @@ $apellidos = $_POST["apellidos"];
 $telefono = $_POST["telefono"];
 $correo = $_POST["correo"];
 $mensaje = $_POST["mensaje"];
+$header = "Mensaje enviado desde mi portafolio";
 $contenido = "Nombre: ". $nombre . "\n\nApellidos: " . $apellidos . "\n\nTelefono: " . $telefono . "\n\nCorreo: " . $correo . "\n\nMensaje: " . $mensaje;
-mail($destino, "Contacto", $contenido)
+mail($destino, "Contacto", $contenido, $header)
 echo"<script>alert('Correo enviado exitosamente, pronto Leonardo se contactará contigo')</script>";
 ?>
